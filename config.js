e21847b29ee92f220cce9448269e5cf07a4c3053
Ansh Tyagi <anshtyagi0093@gmail.com>
2021-11-25 15:51:13 +0530

    Source code of bhbotlist

---
+++ config.js
@@ -0,0 +1,178 @@
+
+module.exports = {
+  bot: {
+    token: "bot toke here",
+    prefix: "-",
+    owners: ["836958855866089512", "671390595184459782", "566113631499059220"],
+    mongourl: "mongodb url here",
+    servers: {
+      token: "serverlist bot here",
+      prefix: "-"
+    }
+  },
+
+  website: {
+    callback: "https://yourwebsite.com/callback",
+    secret: "client secret",
+    clientID: "842752825888342056", // Bot client id.
+    tags: ["Moderation", "Fun", "Minecraft", "Economy", "Guard", "NSFW", "Anime", "Invite", "Music", "Logging", "Web Dashboard", "Reddit", "Youtube", "Twitch", "Crypto", "Leveling", "Game", "Roleplay", "Utility", "India"],
+    languages: [
+      { flag: 'gb', code: 'en', name: 'English' },
+      { flag: 'pt', code: 'pt', name: 'Portuguese' },
+      { flag: 'es', code: 'es', name: 'Spanish' },
+      { flag: 'fr', code: 'fr', name: 'French' },
+      { flag: 'de', code: 'de', name: 'German' },
+      { flag: 'al', code: 'al', name: 'Albanian' },
+      { flag: 'ru', code: 'ru', name: 'Russian' },
+      { flag: 'cn', code: 'cn', name: 'Chinese' },
+      { flag: 'tr', code: 'tr', name: 'Turkish' },
+      { flag: 'in', code: 'in', name: 'Indian' }
+    ],
+    servers: {
+      tags: [
+        {
+          icon: "fal fa-code",
+          name: "Development"
+        },
+        {
+          icon: "fal fa-play",
+          name: "Stream"
+        },
+        {
+          icon: "fal fa-camera",
+          name: "Media"
+        },
+        {
+          icon: 'fal fa-building',
+          name: 'Company'
+        },
+        {
+          icon: 'fal fa-gamepad',
+          name: 'Game'
+        },
+        {
+          icon: 'fal fa-icons',
+          name: 'Emoji'
+        },
+        {
+          icon: 'fal fa-robot',
+          name: 'Bot List'
+        },
+        {
+          icon: 'fal fa-server',
+          name: 'Server List'
+        },
+        {
+          icon: 'fal fa-moon-stars',
+          name: 'Turkish'
+        },
+        {
+          icon: 'fab fa-discord',
+          name: 'Support'
+        },
+        {
+          icon: 'fal fa-volume',
+          name: 'Sound'
+        },
+        {
+          icon: 'fal fa-comments',
+          name: 'Chatting'
+        },
+        {
+          icon: 'fal fa-lips',
+          name: 'NSFW'
+        },
+        {
+          icon: "fal fa-comment-slash",
+          name: "Challange"
+        },
+        {
+          icon: "fal fa-hand-rock",
+          name: "Protest"
+        },
+        {
+          icon: "fal fa-headphones-alt",
+          name: "Roleplay"
+        },
+        {
+          icon: "fal fa-grin-alt",
+          name: "Meme"
+        },
+        {
+          icon: "fal fa-shopping-cart",
+          name: "Shop"
+        },
+        {
+          icon: "fal fa-desktop",
+          name: "Technology"
+        },
+        {
+          icon: "fal fa-laugh",
+          name: "Fun"
+        },
+        {
+          icon: "fal fa-share-alt",
+          name: "Social"
+        },
+        {
+          icon: "fal fa-laptop",
+          name: "E-Spor"
+        },
+        {
+          icon: 'fal fa-palette',
+          name: 'Design'
+        },
+        {
+          icon: 'fal fa-users',
+          name: 'Community'
+        }
+      ]
+    }
+  },
+
+  server: {
+    id: "805130400623362048",
+    invite: "https://discord.gg/7SvnbjGWcX",
+    roles: {
+      administrator: "844906017061208064",
+      moderator: "842764846889959454",
+      profile: {
+        sitecreator: "820942294152642571",
+        booster: "806516373701787689",
+        sponsor: "854618453770895360",
+        supporter: "854623285739913247",
+        partnerRole: "844921888492486686"
+      },
+      codeshare: {
+        javascript: "854618913932181516",
+        html: "854619356142239747",
+        substructure: "854619547957329930",
+        bdfd: "854619668043137064", // Bot Designer For Discord
+        fiveInvite: "",
+        tenInvite: "",
+        fifteenInvite: "",
+        twentyInvite: ""
+      },
+      botlist: {
+        developer: "854622388465303553",
+        certified_developer: "854622657990885396",
+        bot: "845238820751802398",
+        certified_bot: "854623089496948736",
+        bug: "887892978700521523",
+        premium_developer: "889024613252739072",
+        premium_bot: "873689030838091826",
+        staff: "887893412764860416"
+      }
+    },
+    channels: {
+      codelog: "845239798875684904",
+      login: "845238080113606691",
+      webstatus: "843528756882047027",
+      uptimelog: "845238538052304906",
+      botlog: "845239284881293313",
+      votes: "845239378526601237"
+    }
+  }
+
+
+}
