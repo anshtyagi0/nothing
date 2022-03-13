905a5f1d38c9458a7e9ba44a2da0549024fff0d2
Ansh Tyagi <anshtyagi0093@gmail.com>
2022-03-13 21:12:48 +0530

    Update ads.js

--- src/routers/ads.js
+++ src/routers/ads.js
@@ -1,5 +1,5 @@
 const app = require('express').Router();
-const Database = require("void.db");
+const Database = require("bhbl.db");
 const path = require("path")
 
 console.log("[bhbotlist.xyz]: Ads router loaded.");
